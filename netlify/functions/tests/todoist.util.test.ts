2e2f9fee5a9c0143358894f0700f764fa83a8467
Witold Drożdżowski <witold.drozdzowskir@gmail.com>
2024-02-15 14:25:09 +1030

    Fixed filter in api issue.

--- netlify/functions/tests/todoist.util.test.ts
+++ netlify/functions/tests/todoist.util.test.ts
@@ -1,4 +1,4 @@
-import {LocalDate, LocalDateTime} from "@js-joda/core";
+import {LocalDate} from "@js-joda/core";
 import {createDueFilterBetweenDates} from "../../helper/todoist.util";
 
 test('Should correctly transform into url', () => {
@@ -6,5 +6,5 @@ test('Should correctly transform into url', () => {
     const givenDateTo =  LocalDate.parse("2024-02-26");
 
     const createdUrl = createDueFilterBetweenDates(givenDateFrom, givenDateTo);
-    expect(createdUrl).toEqual("due%20after%3A%202024-02-21%200am%20%26%20due%20before%3A%202024-02-27%2000am");
+    expect(createdUrl).toEqual("due after: 2024-02-21 0am & due before: 2024-02-27 0am");
 })
